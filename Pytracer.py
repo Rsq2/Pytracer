92196f3d2707d627e2b288481bafda7e49f88f49
D.J. Dykes <ddykes2@gmail.com>
2015-07-22 11:33:08 -0700

    Unsalvagable. Rewrite and restructure coming soon.

--- Pytracer.py
+++ Pytracer.py
@@ -3,8 +3,6 @@ from PIL import Image
 
 
 #TODO:
-#       implement reflectance
-#           if Sphere.intersection == True, Ray.reflect(maxDepth)       
 
 
 #ABSTRACT:
@@ -46,19 +44,19 @@ class Coordinates( object ):
     
     # method overrides so we can add / subtract / multiply / divide our vector and color tuples
     def __add__(self, b):
-        return Coordinates((self.x + b.x), (self.y + b.y), (self.z + b.z))
+        return Coordinates(self.x + b.x, self.y + b.y, self.z + b.z)
     def __sub__(self, b):
-        return Coordinates((self.x - b.x), (self.y - b.y), (self.z - b.z))
+        return Coordinates(self.x - b.x, self.y - b.y, self.z - b.z)
     def __mul__(self, b):
-        return Coordinates((self.x * b.x), (self.y * b.y), (self.z * b.z))
+        return Coordinates(self.x * b.x, self.y * b.y, self.z * b.z)
     def __div__(self, b):
-        return Coordinates((self.x / b.x), (self.y / b.y), (self.z / b.z))
+        return Coordinates(self.x / b.x, self.y / b.y, self.z / b.z)
     
 class Sphere( object ):
-    def __init__(self, center, radius, texture):
+    def __init__(self, center, radius, color):
         self.center = center
         self.radius = radius
-        self.texture = texture
+        self.texture = color
         
     def Intersection(self, ray):
     # Constructing a Quadratic Equation
@@ -66,7 +64,7 @@ class Sphere( object ):
         a = Coordinates(ray.direction).normalize() * dist
         b = a * a - dist * dist + self.radius * self.radius
         
-        if b < 0.0: #catch for non-intersection
+        if b < 0.0: # catch for non-intersection
             return False
         p1 = a - sqrt(b)
         p2 = a + sqrt(b)
@@ -80,28 +78,27 @@ class Sphere( object ):
             p0 = p2
             returnval = True
         return returnval        
-        Intersect.distance = Ray.point + distance * Ray.normalize() 
 
     def norm(self, n):
         return Coordinates(n - self.center)
 
 class Ray( object ):
-    def __init__(self, point, direction):
-        self.point = point
+    def __init__(self, origin, direction):
+        self.origin = origin
         self.direction = direction
     
     def reflect(self, intersection, direction):          
-        reflect = 2.0 * (direction * n)
+        reflect = 2.0 * (direction * this.normalize())
         newstart = intersection
-        newdir = direction - (reflect * n)
-        rayTest(newstart, newdir)
+        newdir = direction - (reflect * this.normalize())
+        getClosestIntersect(newstart, newdir)
          
-        return # TODO: return color value for incident ray 
-               # define n
+        return # TODO: return color value for incident ray
 
-class lightSource( object ):
-    def __init__(self, center, intensity):
+class Light( object ):
+    def __init__(self, center, color, intensity):
         self.center = center
+        self.color = color
         self.intensity = intensity
 
 class Intersection ( object ):
@@ -110,7 +107,7 @@ class Intersection ( object ):
         self.distance = distance
         self.obj = obj
 
-def rayTest(ray, objects):
+def getClosestIntersect(ray, objects):
     closestIntersect = (Coordinates(0.0,0.0,0.0)
     for obj in objects:
         currentIntersect = obj.Intersection(ray)
@@ -120,16 +117,30 @@ def rayTest(ray, objects):
             closestIntersect = currentIntersect        
         return closestIntersect        
 
-class World()
-    def __init__(self)
-    def 
+def shadowTest(light, intersection, objects):
+    
+
+
+#class World(objects, sky, lights)
+    #def __init__(self)
+        #self.objects = objects
+        #self.sky = sky
+        #self.lights = lights
 
 def scan(x, y):
-    for num in range(x):
+    for num in range(x)
     print num
         for num in range(y):
-            finalColor = 0.0
-            for obj in Objects[] 
-            collide = rayTest(Coordinates(x, y, -1000.0), Objects[])#make an object list)
-            if collide == True
-            img.putpixel((x, y - num), (min(red * 255.0, 255.0), min(blue * 255.0, 255.0), min(green * 255.0, 255.0)))
+            finalColor = (0.0, 0.0, 0.0) #RGB
+            maxDepth = 10
+            currentDepth = 0
+            reflectFactor = 1
+            while currentDepth < maxDepth or 0 < reflectFactor
+                for obj in Objects[] 
+                    closestInt = getClosestIntersect(Coordinates(x, y, -1000.0), Objects[])
+                    for light in Lights[]
+                        if shadowTest(light, closestInt, obj) == False
+                            finalColor = finalColor + computedColor * reflectFactor
+                            reflectFactor = reflectFactor * reflectance
+                            img.putpixel(finalColor)
+                            currentDepth += 1
